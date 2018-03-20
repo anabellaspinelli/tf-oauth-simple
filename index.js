ad3426c75e13786e3393078d4376e569119b712a
Anabella Spinelli <anabella.spinelli@typeform.com>
2018-03-20 17:48:32 +0100

    Auth working finally

--- index.js
+++ index.js
@@ -9,15 +9,22 @@ app.use(bodyParser.json());
 app.get("/callback", (req, res, next) => {
   return request
     .post("https://api.typeform.com/oauth/token")
-    .field("code", req.query.code)
-    .field("client_id", "HZTj8rdGhFkDFfQyZWfkRFPmBQCJ5bnjkSoqBPHM66tH")
-    .field("client_secret", "EFTXokx7mzteXTazav6EjSrpduq8bEPrs4thKsx8ic5N")
+    .type("form")
+    .send({
+      code: req.query.code,
+      client_id: "HZTj8rdGhFkDFfQyZWfkRFPmBQCJ5bnjkSoqBPHM66tH",
+      client_secret: "EFTXokx7mzteXTazav6EjSrpduq8bEPrs4thKsx8ic5N",
+      redirect_uri: "https://typeform-js-oauth.herokuapp.com/callback"
+    })
     .then(r => {
-      console.log("aventura");
-      return res.send(r.body);
+      return res.send(`
+        <p>Your token is <span style="color: green">${
+          r.body.access_token
+        }</span></p>
+      `);
     })
     .catch(err => {
-      console.error(err.body);
+      console.error(err);
     });
 });
 
